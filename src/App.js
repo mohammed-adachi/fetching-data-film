ec4df7024aa257d74d444bbe6b73893fd5548bb1
Mohamed <mohame@gmail.com>
2024-02-22 15:53:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Film from './movie.jsx';
 
-function App() {
+const API_Key = 'http://www.omdbapi.com?apikey=6a4408af'; // URL complète pour l'API Key
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    try {
+      const response = await fetch(`${API_Key}&s=${title}`); // Utilisation du titre directement dans l'URL
+      const data = await response.json();
+      setMovies(data.Search || []); // Utilisation de data.Search ou un tableau vide si data.Search est undefined
+    } catch (error) {
+      console.error('Erreur lors de la récupération des films:', error);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies(searchTerm); // Appeler searchMovies avec le terme de recherche actuel
+  }, [searchTerm]); // Exécuter useEffect lorsque searchTerm change
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Les films</h1>
+      <div className='search'>
+        <input placeholder="Recherche sur le film" onChange={e => setSearchTerm(e.target.value)}></input>
+        <img
+        src='srcIcon'
+        alt='cherche'
+        onClick={() => {}}>
+      </img>
+      </div>
+      
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, index) => (
+            <Film key={index} movie={movie} /> // Ajouter une clé unique pour chaque composant Film
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>Aucun film trouvé</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/movie.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const movie=({movie})=>{
+    return(
+        <div className='movie'>
+          <div>   
+      <p>{movie.Year}</p>
+      </div>
+      <div>
+      <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}></img>
+          </div>
+          <div>
+          <span >{movie.Type}
+          <h3>{movie.Title}</h3></span>
+      </div>
+      </div>
+    )
+}
+export default movie;
